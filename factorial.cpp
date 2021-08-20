ce924f53f086838ed926ea0fbb1d85ae0e957f81
Srishti Ginjala <srishti.ginjala@gmail.com>
2021-08-20 15:04:23 +0530

    again modified factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 using namespace std;
 
+//git is interesting
+
 int factorial(int n){
+    if(n==0)
+    return 1;
     while(n<0)
     {   int num;
         cout<<"Please enter a positive number\n";
