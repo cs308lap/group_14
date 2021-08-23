249d04c46a29780549706537f92e369de146396c
Mayank Jindal <m.mayankj.jindal@gmail.com>
2021-08-23 15:30:28 +0530

    i

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     print_hello();
     cout << endl;
-    cout << "The factorial of 5 is " << factorial(5) << endl;
+    cout << "hey welcome to new branch\nThe factorial of 5 is " << factorial(5) << endl;
     return 0;
 }
